8ddd1cdc2545bcb6b32f3a9b539b63d20261365f
Paymir121 <nikox12@mail.ru>
2022-10-02 13:16:45 +0300

    final 1

---
+++ yatube/groups/urls.py
@@ -0,0 +1,14 @@
+from django.urls import path
+
+from . import views
+
+app_name = 'groups'
+
+urlpatterns = [
+    # Главная страница
+    path('', views.index),
+    # Список мороженого
+    
+    # Подробная информация о мороженом. Ждем пременную типа int,
+    # и будем использовать ее под именем pk
+]
