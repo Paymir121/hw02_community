fba002410f83d0711482e1d8e994e9d75b623f12
paymir121 <nikox12@mail.ru>
2022-10-07 13:54:16 +0300

    commit branch in work

--- yatube/yatube/urls.py
+++ yatube/yatube/urls.py
@@ -21,4 +21,6 @@ urlpatterns = [
     path('', include('posts.urls', namespace='post')),
     path('', include('groups.urls')),
     path('admin/', admin.site.urls),
+    path('auth/', include('users.urls')), 
+    path('auth/', include('django.contrib.auth.urls')),
 ]
