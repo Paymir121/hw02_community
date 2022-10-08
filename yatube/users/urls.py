701d6541668ec12ccab2f07c7d19d69664658549
paymir121 <nikox12@mail.ru>
2022-10-08 15:14:46 +0300

    i molodec

--- yatube/users/urls.py
+++ yatube/users/urls.py
@@ -1,4 +1,4 @@
-from django.contrib.auth.views import LoginView,  LogoutView, PasswordResetView
+from django.contrib.auth.views import LoginView,  LogoutView, PasswordResetView, PasswordChangeView, PasswordChangeDoneView, PasswordResetDoneView,PasswordResetCompleteView , PasswordResetConfirmView
 from django.urls import path
 from . import views
 
@@ -17,8 +17,33 @@ urlpatterns = [
         name='login'
     ),
     path(
-        'password_reset_form/',
-        PasswordResetView.as_view(template_name='users/login.html'),
+        'password_reset/',
+        PasswordResetView.as_view(template_name='users/password_reset_form.html'),
         name='password_reset_form'
+    ),
+    path(
+        'password_reset/done/',
+        PasswordResetDoneView.as_view(template_name='users/password_reset_done.html'),
+        name='password_reset_form'
+    ),
+    path(
+        'password_change/',
+        PasswordChangeView.as_view(template_name='users/password_change.html'),
+        name='password_change_form'
+    ),
+    path(
+        'password_change/done/',
+        PasswordChangeDoneView.as_view(template_name='users/password_change_done.html'),
+        name='password_change_done'
+    ),
+    path(
+        'reset/done/<uidb64>/<token>/',
+        PasswordResetConfirmView.as_view(template_name='users/password_reset_confirm.html'),
+        name='password_reset_confirm'
+    ),
+    path(
+        'reset/done/',
+        PasswordResetCompleteView.as_view(template_name='users/password_reset_complete.html'),
+        name='password_reset_complete'
     ),
 ] 
